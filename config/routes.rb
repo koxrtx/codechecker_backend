343eb5d52d5aae18d4e6312d8b7384ddc4fbf636
koxrtx <koxrtx2003@yahoo.co.jp>
2025-06-17 14:21:03 +0900

    なんか色々した

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "posts#index"
+  resources :users
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
 end
