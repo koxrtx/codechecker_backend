2b715ecb40eed41313fe8eab2a98d9aba604d903
koxrtx <koxrtx2003@yahoo.co.jp>
2025-06-19 15:14:10 +0900

    ライセンス記載

--- app/controllers/openai/problems_controller.rb
+++ app/controllers/openai/problems_controller.rb
@@ -1,6 +1,16 @@
 class Openai::ProblemsController < ApplicationController
   def daily
-    @problem = ProblemGenerator.generate_daily_problem
+    begin
+      @problem = ProblemGenerator.generate_daily_problem
+    rescue OpenAI::Errors::APIConnectionError => e
+      puts ("サーバーに接続できませんでした")
+      puts (e.cause)
+    rescue OpenAI::Errors::RateLimitError => e
+      puts ("439 status しばらく待ってください")
+    rescue OpenAI::Errors::APIStatusError => e
+      puts ("200番台以外のステータスコードが返されました")
+      puts (e.status)
+    end
 
     render :daily
   end
